afe36a66ec1ca0150120e6541926e788b5a4db18
ahrenswett <ahrenswett@gmail.com>
2019-05-06 20:41:04 -0700

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+/* var likesGameOfThrones = prompt('Do you like Game of Thrones?');
+
+likesGameOfThrones = likesGameOfThrones.toLowerCase();
+
+if (likesGameOfThrones === "yes" ){
+    alert('Me Too!');
+}
+
+else {
+    alert('How can you not like Game of Thrones!');
+}
+
+*/
+
+var gender = prompt('Are you male or Female?');
+
+gender = gender.toLowerCase();
+
+if(gender === 'male'){
+    alert('Awesome Guys Rock!');
+}
+
+else if(gender === 'female'){
+    alert('Sweet girls kick ass!');
+}
+
+else{
+    alert('Its ok im not sure of my gender either');
+}
