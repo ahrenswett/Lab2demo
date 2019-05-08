4f4b22916f47bac3b6e07aef70d0d9072d0ebf10
ahrenswett <ahrenswett@gmail.com>
2019-05-07 17:49:10 -0700

    begining of logic

--- js/app.js
+++ js/app.js
@@ -1,16 +1,4 @@
-/* var likesGameOfThrones = prompt('Do you like Game of Thrones?');
-
-likesGameOfThrones = likesGameOfThrones.toLowerCase();
-
-if (likesGameOfThrones === "yes" ){
-    alert('Me Too!');
-}
-
-else {
-    alert('How can you not like Game of Thrones!');
-}
-
-*/
+/*
 
 var gender = prompt('Are you male or Female?');
 
@@ -27,3 +15,55 @@ else if(gender === 'female'){
 else{
     alert('Its ok im not sure of my gender either');
 }
+
+*/
+
+// counts right and wrong answers
+var right = 0;
+var wrong = 0;
+
+/* converts answer to lowercase and truncates to single letter answer
+function shrink(answer){
+    answer.toLocaleLowerCase()
+    if (answer === yes){
+        answer = y;
+    }
+    else if(answer === no){
+        answer = n;
+    }; 
+*/
+
+
+
+// Asks User if they think I have Kids then logs the answer in console and converts answer to lowercase
+var haveKids = prompt('do you think I have kids?');
+console.log('the user guessed ' + haveKids + ' kids.' );
+haveKids =haveKids.toLocaleLowerCase(); // make into function
+
+// check to see if answer is yes or no changes to y or n 
+if (haveKids === yes){
+    haveKids = y;
+}
+else if(haveKids === no){
+    haveKids = n;
+};
+
+//increments whether if the answer is right or wrong
+if (haveKids === y){
+    right ++
+}
+else{
+    wrong ++
+};
+
+
+
+
+// Asks User if they think I like ice climbing then logs the answer in console
+var iceClimb = prompt('Do you think that I like ice climbing?');
+console.log('the user guessed ' + iceClimb + 'ice climbing.' );
+
+
+// Asks User if they think I ride a vespa then logs the answer in console
+var vespa = prompt('Do you think I ride a vespa?');
+console.log('the user thinks ' + vespa + ' vespa rider')
