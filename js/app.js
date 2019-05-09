07b05c056fbaab37d2037977a3114dbd4846b6d5
ahrenswett <ahrenswett@gmail.com>
2019-05-08 17:05:31 -0700

    all questions now returning proper answer in vertical band.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
 var right = 0;
 var wrong = 0;
 var heldAnswer;
-//not used other tan to track how many times function is called yet but works as counter
+//not used other than to track how many times function is called yet, but works as counter
 var loopCount = 0 ;
 
 
@@ -37,7 +37,7 @@ shrink(haveKids);
 if (heldAnswer === 'y'){
   right++;
   alert('Do you know them?');
-  document.getElementById('question1').innerHTML = 'Question 1: Do you think I have kids?<br>You answered ' + haveKids + ' I have kids.<br>That is correct! I have 3!';
+  document.getElementById('question1').innerHTML = 'Question 1: Do you think I have kids?<br>You answered ' + haveKids + '.<br>That is correct! I have 3!';
 }
 
 else{
@@ -47,9 +47,7 @@ else{
   }
 
   alert('Are you sure about that answer?');
-  document.getElementById('question1').innerHTML = 'Question 1: Do you think I have kids?<br>You answered ' + haveKids + ' I have kids.<br>That is wrong! I have 3!';
-  // resets held answer to empty string
-  heldAnswer = '';
+  document.getElementById('question1').innerHTML = 'Question 1: Do you think I have kids?<br>You answered ' + haveKids + '.<br>That is wrong! I have 3!';
 }
 
 
@@ -70,7 +68,7 @@ shrink(iceClimb);
 if (heldAnswer === 'y'){
   right ++;
   alert('Do you want to come with me?');
-  document.getElementById('question2').innerHTML= 'Question 2: Do you think that I like ice climbing? You answered ' + iceClimb + ' <br>That is correct! Its Magical, So whats not to like!!';
+  document.getElementById('question2').innerHTML= 'Question 2: Do you think that I like ice climbing? You answered ' + iceClimb + ' .<br>That is correct! Its Magical, So whats not to like!!';
 }
 else{
   wrong ++;
@@ -80,7 +78,7 @@ else{
 
 
   alert('Would you bet your life on it?');
-  document.getElementById('question2').innerHTML = 'Question 2: Do you think that I like ice climbing?<br>You answered ' + iceClimb + ' <br>That is Wrong! Its Magical, So whats not to like!!';
+  document.getElementById('question2').innerHTML = 'Question 2: Do you think that I like ice climbing?<br>You answered ' + iceClimb + ' .<br>That is Wrong! Its Magical, So whats not to like!!';
 }
 
 
@@ -98,7 +96,7 @@ shrink(vespa);
 if (heldAnswer === 'n'){
   right ++;
   alert('Correct I ride motorscycles but mine is broken :(');
-  document.getElementById("question3").innerHTML='<br>You answered ' + + ' <br>That is correct! ';
+  document.getElementById('question3').innerHTML='Question 3: Do you think I ride a vespa?<br>You answered ' + vespa + ' <br>That is correct! I have a broken CB750 Cafe Racer.';
 }
 else{
   wrong ++;
@@ -106,7 +104,7 @@ else{
     vespa = ' no answer ';
   }
   alert('put put put nope WRONG!');
-  document.getElementById("question3").innerHTML='<br>You answered ' + + ' <br>That is Wrong!  ';
+  document.getElementById('question3').innerHTML='Question 3: Do you think I ride a vespa?<br>You answered ' + vespa + ' <br>That is Wrong!  I have a broken CB750 Cafe Racer.';
 }
 
 
@@ -122,13 +120,18 @@ shrink(cityFrom);
 //increments a counter if the answer is right or wrong
 if (heldAnswer === 'y'){
   right ++;
-  alert('Are you from there too?')
-  document.getElementById("question2").innerHTML='<br>You answered ' + + ' <br>That is correct! ';
+  alert('Are you from there too?');
+  document.getElementById('question4').innerHTML='Question 4: Do you think Im from Boulder,Co?<br>You answered ' + cityFrom + ' <br>That is correct! ';
 }
+
 else{
   wrong ++;
+  if(heldAnswer === ''){
+    cityFrom = ' no answer ';
+  }
   alert('Wrong-o!');
-  document.getElementById("question2").innerHTML='<br>You answered ' + + ' <br>That is Wrong!  ';
+
+  document.getElementById('question4').innerHTML='Question 4: Do you think Im from Boulder,Co?<br>You answered ' + cityFrom + ' <br>That is Wrong!  ';
 }
 
 
@@ -141,14 +144,20 @@ console.log('the user thinks ' + anotherLang + ' another language');
 // Uses shrink function to convert answer to lowercase and truncates to single letter answer
 shrink(anotherLang);
 
-//increments a counter if the answer is right or wrong
+//increments a counter if the answer is right or wrong Question 5: Do you think I speak another language?
 if (heldAnswer === 'n'){
   right ++;
   alert('Right!I speak some Korean but not fluently, so the answer is no');
+  document.getElementById('question5').innerHTML='Question 5: Do you think I speak another language?<br>You answered ' + anotherLang + ' <br>That is correct! I only speak a little Korean. Not enough to count.';
 }
 else{
   wrong ++;
-  alert('Sorry buddy, I know a little Korean but not enough to count!')
+  if(heldAnswer === ''){
+    anotherLang = ' no answer ';
+  }
+  alert('Sorry buddy, I know a little Korean but not enough to count!');
+  document.getElementById('question5').innerHTML='Question 5: Do you think I speak another language?<br>You answered ' + anotherLang + ' <br>That is wrong! I only speak a little Korean. Not enough to count.';
+
 }
 
 
