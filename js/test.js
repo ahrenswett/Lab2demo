1cd690ed4dbf27b473a7c981d08404559caa6507
ahrenswett <ahrenswett@gmail.com>
2019-05-15 14:31:17 -0700

    All Q's in functions added 5q's to an array shortend code be 181 lines. still need to get number guessing game to recognize correct guess.

--- js/test.js
+++ js/test.js
@@ -33,67 +33,41 @@ var loopCount = 0 ;
 var userName = prompt('Hi, welcome to my page what is your name?');
 console.log('the users name is ' + userName);
 
-//******KIDS********
-// Asks User if they think I have Kids then logs the answer in console
-//var haveKids = prompt('Do you think I have kids?');
-//console.log('the user guessed ' + haveKids + ' kids.' );
-
-// Uses shrink function to convert answer to lowercase and truncates to single letter answer
-//shrink(haveKids);
-
-//increments a counter if the answer is right or wrong
-// if (userAnswer === 'y'){
-//   right++;
-//   alert('Do you know them?');
-//   document.getElementById('question1').innerHTML = 'Question 1: Do you think I have kids?<br><strong>You answered ' + haveKids + '.<br></strong>That is correct! I have 3!';
-// }
-// else
-//   {
-//     wrong++;
-//     if(userAnswer === '')
-//     {
-//       haveKids= ' no answer ';
-//     }
-
-//     alert('Are you sure about that answer?');
-//     document.getElementById('question1').innerHTML = 'Question 1: Do you think I have kids?<br><strong>You answered ' + haveKids + '.</strong><br>That is wrong! I have 3!';
-//   }
-// }
-
 var questions =
 [
-  ['Do you think I have kids?', 'y', 'Thats right I have 3! Micah Aliyah and Autumn', 'Are you sure about that answer?']
-    ['Do you think that I like ice climbing?', 'y', 'Do you want to come with me?','Would you bet your life on it?']
-    ['Do you think I ride a vespa?','Correct I ride motorscycles but mine is broken','put put put nope!']
-    ['Do you think Im from Boulder,Co?','Are you from there too?','Wrong-o!']
-    ['Do you think I speak another language?','Right!I speak some Korean but not fluently, so the answer is no','Sorry buddy, I know a little Korean but not enough to count!']
+  ['Do you think I have kids?', 'y', 'Thats right I have 3! Micah Aliyah and Autumn', 'Are you sure about that answer?'],
+  ['Do you think that I like ice climbing?', 'y', 'Do you want to come with me?','Would you bet your life on it?'],
+  ['Do you think I ride a vespa?','n','Correct I ride motorscycles but mine is broken','put put put nope!'],
+  ['Do you think Im from Boulder,Co?','y','Are you from there too?','Wrong-o!'],
+  ['Do you think I speak another language?','n','Right!I speak some Korean but not fluently, so the answer is no','Sorry buddy, I know a little Korean but not enough to count!']
 ];
 
 //var questionGameRightGuesses = [];
 
-for(var j = 0; j < 6; j++ )
+for(var j = 0; j < 5; j++ )
 {
-  userAnswer = prompt(questions[j][1]);
+  userAnswer = prompt(questions[j][0]);
   shrink(userAnswer);
-  console.log('the user guessed ' + questions[j][1] + ' ' + shrink(userAnswer));
-  
+  console.log('the user guessed ' + questions[j][0] + ' ' + userAnswer);
+
   if(userAnswer === '')
   {
-    prompt('You did not enter a response, Please resubmit your answer.');
-    j--;
+    prompt('You did not enter a response');
   }
-  else if (userAnswer === questions[j][2])
+  else if (userAnswer === questions[j][1])
   {
     right++;
-    alert(questions[j][3]);
-    questions.push(userAnswer);
+    alert(questions[j][2]);
+    questions[j].push(userAnswer);
+    console.log(questions[j])
   }
   else
   {
     wrong++;
-    alert(questions[j][4]);
+    alert(questions[j][3]);
   }
 }
 
-console.log('You got ' + right + ', out of 5.');
-console.log('You got ' + wrong + ', out of 5.');
+console.log('You got ' + right + ', out of 5 right');
+console.log('You got ' + wrong + ', out of 5 wrong');
+console.log('users answers = '+ questions[0][4]+' '+questions[1][4]+' '+questions[2][4]+' '+questions[3][4]);
